8c4d042766fcc42ede91a36eaa8c6804b539efec
Mahalakshmaiah Gurram <mahalakshmaiah7@gmail.com>
2020-09-16 12:22:59 +0530

    initial commit

---
+++ src/main/java/com/maha/configclient/SpringBootCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.maha.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maha/configclient/SpringBootCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maha.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
